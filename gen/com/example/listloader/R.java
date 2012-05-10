6025ca2e147ca76f7d08fdf28e5bcbad6cf20990
Don Archer <don.archer@verizon.com>
2012-05-10 15:37:57 -0500

    using custom adapter to illustrate setTag

--- gen/com/example/listloader/R.java
+++ gen/com/example/listloader/R.java
@@ -14,10 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int fragholder=0x7f050000;
+        public static final int fragholder=0x7f050002;
+        public static final int text1=0x7f050000;
+        public static final int text2=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
